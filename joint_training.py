b7edbc915e0858418fe0e777b9700f898813d865
Lucas <c.oliveira25@pucpr.edu.br>
2026-03-09 16:33:45 -0300

    ADD: joint training

--- joint_training.py
+++ joint_training.py
@@ -17,51 +17,32 @@ valid_loader = DataLoader(valid, batch_size=32, shuffle=False)
 test_loader = DataLoader(test, batch_size=32, shuffle=False)
 
 
- 
-# Define models
-class SimpleCNN(nn.Module):
-    def __init__(self):
-        super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(16 * 32 * 32, 10)
- 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = x.view(-1, 16 * 32 * 32)
-        x = self.fc1(x)
-        return x
+from src.models.classifier import Classifier
+from src.models.autoencoders_with_skip_connections import SkipEncoder0, SkipEncoder1
+from src.config.config import Config
+
+config = Config()
+device = config.DEVICE0
  
  
-class SimpleRNN(nn.Module):
-    def __init__(self):
-        super(SimpleRNN, self).__init__()
-        self.rnn = nn.RNN(10, 20, 1, batch_first=True)
-        self.fc = nn.Linear(20, 10)
+skip_0 = SkipEncoder0()
+skip_0.load_state_dict(torch.load('/home/lucas.ocunha/ConditionalAutoencoder/models/SkipAutoencoder0/CNR/encoder.pth', map_location=device))
+skip_0.to(device)
+model_0 = Classifier(skip_0, latent_dim=skip_0.latent_dim, num_classes=2)
  
-    def forward(self, x):
-        out, _ = self.rnn(x)
-        out = self.fc(out[:, -1, :])
-        return out
+skip_1 = SkipEncoder1()
+skip_1.load_state_dict(torch.load('/home/lucas.ocunha/ConditionalAutoencoder/models/SkipAutoencoder1/CNR/encoder.pth', map_location=device))
+skip_1.to(device)
+model_1 = Classifier(skip_1, latent_dim=skip_1.latent_dim, num_classes=2)
  
- 
-cnn = SimpleCNN()
-rnn = SimpleRNN()
+
  
 # Initialize parameters
 import torch.nn.init as init
  
-for m in cnn.modules():
+for m in model_0.modules():
     if isinstance(m, nn.Conv2d):
         init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-    elif isinstance(m, nn.Linear):
-        init.xavier_normal_(m.weight)
- 
-for m in rnn.modules():
-    if isinstance(m, nn.RNN):
-        init.orthogonal_(m.weight_hh_l0)
-        init.xavier_normal_(m.weight_ih_l0)
     elif isinstance(m, nn.Linear):
         init.xavier_normal_(m.weight)
  
@@ -70,56 +51,51 @@ criterion_cnn = nn.CrossEntropyLoss()
 criterion_rnn = nn.CrossEntropyLoss()
  
  
-def combined_loss(output_cnn, output_rnn, target):
-    loss_cnn = criterion_cnn(output_cnn, target)
-    loss_rnn = criterion_rnn(output_rnn, target)
+def combined_loss(output_model_0, output_model_1, target):
+    loss_cnn = criterion_cnn(output_model_0, target)
+    loss_rnn = criterion_rnn(output_model_1, target)
     return loss_cnn + loss_rnn
  
  
 # Define optimizer
-parameters = list(cnn.parameters()) + list(rnn.parameters())
+parameters = list(model_0.parameters()) + list(model_1.parameters())
 optimizer = optim.Adam(parameters, lr=0.001)
 scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
  
-# TensorBoard writer
-writer = SummaryWriter()
- 
-# Training data
-input_cnn = torch.randn(10, 3, 32, 32)
-input_rnn = torch.randn(10, 5, 10)
-target = torch.randint(0, 10, (10,))
  
 num_epochs = 10
 best_val_loss = float('inf')
 patience = 5
 counter = 0
 for epoch in range(num_epochs):
-    optimizer.zero_grad()
+    
+    for x, y, _ in train_loader:
+        x, y = x.to(device), y.to(device)
+        
+        output_model_0 = model_0(x)
+        output_model_1 = model_1(x)
+    
+        loss = combined_loss(output_model_0, output_model_1, y)
+
+        optimizer.zero_grad()
  
-    output_cnn = cnn(input_cnn)
-    output_rnn = rnn(input_rnn)
+        
+        loss.backward()
+    
+        # Gradient clipping
+        torch.nn.utils.clip_grad_norm_(parameters, max_norm=1.0)
+    
+        optimizer.step()
+        scheduler.step()
+    
+    
+    for x, y, _ in valid_loader:
+        x, y = x.to(device), y.to(device)
+        
+        output_model_0 = model_0(x)
+        output_model_1 = model_1(x)
+    
+        val_loss = combined_loss(output_model_0, output_model_1, y)
  
-    loss = combined_loss(output_cnn, output_rnn, target)
-    loss.backward()
+    print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item()}, Val Loss: {val_loss.item()}')
  
-    # Gradient clipping
-    torch.nn.utils.clip_grad_norm_(parameters, max_norm=1.0)
- 
-    optimizer.step()
-    scheduler.step()
- 
-    writer.add_scalar('Loss/train', loss.item(), epoch)
- 
-    # Simulate validation loss
-    val_loss = loss.item()
-    if val_loss < best_val_loss:
-        best_val_loss = val_loss
-        counter = 0
-    else:
-        counter += 1
-        if counter >= patience:
-            break
- 
-    print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item()}')
- 
-writer.close()
