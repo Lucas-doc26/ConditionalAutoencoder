9b82cfcc180fa0a8d148e39bc32e4ea7d4b27680
lucas <lucas.ocunha@ppgia.pucpr.br>
2026-03-09 17:04:33 -0300

    ADD: primeira versao do joint training

--- joint_training.py
+++ joint_training.py
@@ -1,15 +1,26 @@
+import pandas as pd 
 from src.utils.datasets import CustomImageDataset
 
+from tqdm import tqdm
 import torch 
 from torch.utils.data import DataLoader
 import torch.nn as nn
 
 import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
-from torch.utils.tensorboard import SummaryWriter
+
+from sklearn.metrics import accuracy_score
+
+from src.models.classifier import Classifier
+from src.models.autoencoders_with_skip_connections import SkipEncoder0, SkipEncoder1
+from src.config.config import Config
+
+import torch.nn.init as init
+
+#ref: https://www.codegenes.net/blog/how-to-do-joint-training-on-many-models-pytorch/
 
 train = CustomImageDataset(csv="/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/batches/batch-1024.csv", autoencoder=False)
-valid = CustomImageDataset(csv="/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/PUC_valid.csv", autoencoder=False)
+valid = CustomImageDataset(csv="/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/PUC_validation.csv", autoencoder=False)
 test = CustomImageDataset(csv="/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/PUC_test.csv", autoencoder=False)
 
 train_loader = DataLoader(train, batch_size=32, shuffle=True)
@@ -17,9 +28,6 @@ valid_loader = DataLoader(valid, batch_size=32, shuffle=False)
 test_loader = DataLoader(test, batch_size=32, shuffle=False)
 
 
-from src.models.classifier import Classifier
-from src.models.autoencoders_with_skip_connections import SkipEncoder0, SkipEncoder1
-from src.config.config import Config
 
 config = Config()
 device = config.DEVICE0
@@ -29,16 +37,16 @@ skip_0 = SkipEncoder0()
 skip_0.load_state_dict(torch.load('/home/lucas.ocunha/ConditionalAutoencoder/models/SkipAutoencoder0/CNR/encoder.pth', map_location=device))
 skip_0.to(device)
 model_0 = Classifier(skip_0, latent_dim=skip_0.latent_dim, num_classes=2)
- 
+model_0.to(device)
+
 skip_1 = SkipEncoder1()
 skip_1.load_state_dict(torch.load('/home/lucas.ocunha/ConditionalAutoencoder/models/SkipAutoencoder1/CNR/encoder.pth', map_location=device))
 skip_1.to(device)
 model_1 = Classifier(skip_1, latent_dim=skip_1.latent_dim, num_classes=2)
- 
+model_1.to(device) 
 
  
 # Initialize parameters
-import torch.nn.init as init
  
 for m in model_0.modules():
     if isinstance(m, nn.Conv2d):
@@ -63,7 +71,7 @@ optimizer = optim.Adam(parameters, lr=0.001)
 scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
  
  
-num_epochs = 10
+num_epochs = 100
 best_val_loss = float('inf')
 patience = 5
 counter = 0
@@ -98,4 +106,23 @@ for epoch in range(num_epochs):
         val_loss = combined_loss(output_model_0, output_model_1, y)
  
     print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item()}, Val Loss: {val_loss.item()}')
- 
+
+
+y_pred_model_0 = []
+y_pred_model_1 = []
+y_true = pd.read_csv("/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/PUC_test.csv")["class"].values.tolist()
+model_0.eval()
+model_1.eval()
+with torch.no_grad():
+    for x, y, _ in tqdm(test_loader, desc='Test'):
+        x, y = x.to(device), y.to(device)
+        
+        output_model_0 = model_0(x)
+        output_model_1 = model_1(x)
+    
+        y_pred_model_0.extend(output_model_0.argmax(dim=1).cpu().numpy())
+        y_pred_model_1.extend(output_model_1.argmax(dim=1).cpu().numpy())
+
+
+print("Model 0 Accuracy:", accuracy_score(y_true, y_pred_model_0))
+print("Model 1 Accuracy:", accuracy_score(y_true, y_pred_model_1))  
