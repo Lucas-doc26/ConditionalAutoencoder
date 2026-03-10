b558ee9eb88d24e837a7e97fe99a736245acb4dd
Lucas <c.oliveira25@pucpr.edu.br>
2026-03-10 17:17:05 -0300

    ADD: experimento do joint autoencoder

--- joint_training.py
+++ joint_training.py
@@ -12,121 +12,121 @@ from torch.optim.lr_scheduler import StepLR
 from sklearn.metrics import accuracy_score
 
 from src.models.classifier import Classifier
-from src.models.autoencoders_with_skip_connections import SkipEncoder0, SkipEncoder1
+from src.models.joint_autoencoder import JointAutoencoder0, JointAutoencoder1
 from src.config.config import Config
+from src.utils.plot import plot_reconstruction
 
 import torch.nn.init as init
 
 #ref: https://www.codegenes.net/blog/how-to-do-joint-training-on-many-models-pytorch/
 
-train = CustomImageDataset(csv="/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/batches/batch-1024.csv", autoencoder=False)
-valid = CustomImageDataset(csv="/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/PUC_validation.csv", autoencoder=False)
-test = CustomImageDataset(csv="/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/PUC_test.csv", autoencoder=False)
+train = CustomImageDataset(csv="/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/batches/batch-1024.csv", autoencoder=True)
+valid = CustomImageDataset(csv="/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/PUC_validation.csv", autoencoder=True)
+test = CustomImageDataset(csv="/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/PUC_test.csv", autoencoder=True)
 
 train_loader = DataLoader(train, batch_size=32, shuffle=True)
 valid_loader = DataLoader(valid, batch_size=32, shuffle=False)
 test_loader = DataLoader(test, batch_size=32, shuffle=False)
 
 
-
 config = Config()
 device = config.DEVICE0
  
- 
-skip_0 = SkipEncoder0()
-skip_0.load_state_dict(torch.load('/home/lucas.ocunha/ConditionalAutoencoder/models/SkipAutoencoder0/CNR/encoder.pth', map_location=device))
-skip_0.to(device)
-model_0 = Classifier(skip_0, latent_dim=skip_0.latent_dim, num_classes=2)
-model_0.to(device)
+joint_0 = JointAutoencoder0()
+joint_1 = JointAutoencoder1()
 
-skip_1 = SkipEncoder1()
-skip_1.load_state_dict(torch.load('/home/lucas.ocunha/ConditionalAutoencoder/models/SkipAutoencoder1/CNR/encoder.pth', map_location=device))
-skip_1.to(device)
-model_1 = Classifier(skip_1, latent_dim=skip_1.latent_dim, num_classes=2)
-model_1.to(device) 
+joint_0.to(device)
+joint_1.to(device)
 
- 
- 
-for m in model_0.modules():
-    if isinstance(m, nn.Conv2d):
-        init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-    elif isinstance(m, nn.Linear):
-        init.xavier_normal_(m.weight)
 
-for m in model_1.modules():
-    if isinstance(m, nn.Conv2d):
-        init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-    elif isinstance(m, nn.Linear):
-        init.xavier_normal_(m.weight)
- 
-criterion_model_0 = nn.CrossEntropyLoss()
-criterion_model_1 = nn.CrossEntropyLoss()
- 
+criterion_model_0 = nn.MSELoss()
+criterion_model_1 = nn.MSELoss()
+
+from src.models.loss.autoencoder_loss import ssim_loss, euclidean_distance_loss
  
 def combined_loss(output_model_0, output_model_1, target):
-    loss_model_0 = criterion_model_0(output_model_0, target)
-    loss_model_1 = criterion_model_1(output_model_1, target)
-    return (0.2 * loss_model_0) + (0.8 * loss_model_1)
- 
+    loss_mse_0 = criterion_model_0(output_model_0, target)
+    loss_mse_1 = criterion_model_1(output_model_1, target)
+
+    ssim_loss_value_0 = ssim_loss(output_model_0, target)
+    ssim_loss_value_1 = ssim_loss(output_model_1, target)
+    
+    euclidean_distance_loss_value = euclidean_distance_loss(output_model_0, output_model_1)
+    
+    total_loss = ((loss_mse_0 * 0.6 + ssim_loss_value_0 * 0.4) + (loss_mse_1 * 0.6 + ssim_loss_value_1 * 0.4)) * 0.5 + euclidean_distance_loss_value * 0.5
+    return total_loss
+
  
 # Define optimizer
-parameters = list(model_0.parameters()) + list(model_1.parameters())
-optimizer = optim.Adam(parameters, lr=0.001)
+parameters = list(joint_0.parameters()) + list(joint_1.parameters())
+optimizer = torch.optim.Adam(parameters)
 scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
  
  
-num_epochs = 200
-best_val_loss = float('inf')
-patience = 5
-counter = 0
-for epoch in range(num_epochs):
+num_epochs = 20
+for epoch in tqdm(range(num_epochs), desc='Epochs'):
+    
+    joint_0.train()
+    joint_1.train()
+    
+    running_loss = 0.0
     
     for x, y, _ in train_loader:
         x, y = x.to(device), y.to(device)
         
-        output_model_0 = model_0(x)
-        output_model_1 = model_1(x)
+        output_joint_0 = joint_0(x)
+        output_joint_1 = joint_1(x)
     
-        loss = combined_loss(output_model_0, output_model_1, y)
+        loss = combined_loss(output_joint_0, output_joint_1, y)
+        running_loss += loss.item()
 
         optimizer.zero_grad()
  
-        
         loss.backward()
     
         # Gradient clipping
         torch.nn.utils.clip_grad_norm_(parameters, max_norm=1.0)
     
         optimizer.step()
-        scheduler.step()
     
+    epoch_loss = running_loss / len(train_loader)
+    scheduler.step()
     
-    for x, y, _ in valid_loader:
-        x, y = x.to(device), y.to(device)
+    joint_0.eval()
+    joint_1.eval()
+    
+    running_val_loss = 0.0
+    
+    with torch.no_grad():
+        for x, y, _ in valid_loader:
+            x, y = x.to(device), y.to(device)
+            
+            output_joint_0 = joint_0(x)
+            output_joint_1 = joint_1(x)
         
-        output_model_0 = model_0(x)
-        output_model_1 = model_1(x)
+            val_loss = combined_loss(output_joint_0, output_joint_1, y)
+            running_val_loss += val_loss.item()
     
-        val_loss = combined_loss(output_model_0, output_model_1, y)
+    val_epoch_loss = running_val_loss / len(valid_loader)
  
-    print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item()}, Val Loss: {val_loss.item()}')
+    print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {epoch_loss:.4f}, Val Loss: {val_epoch_loss:.4f}')
 
 
-y_pred_model_0 = []
-y_pred_model_1 = []
-y_true = pd.read_csv("/home/lucas.ocunha/ConditionalAutoencoder/CSV/PUC/PUC_test.csv")["class"].values.tolist()
-model_0.eval()
-model_1.eval()
+joint_0.eval()
+joint_1.eval()
+
 with torch.no_grad():
-    for x, y, _ in tqdm(test_loader, desc='Test'):
-        x, y = x.to(device), y.to(device)
-        
-        output_model_0 = model_0(x)
-        output_model_1 = model_1(x)
-    
-        y_pred_model_0.extend(output_model_0.argmax(dim=1).cpu().numpy())
-        y_pred_model_1.extend(output_model_1.argmax(dim=1).cpu().numpy())
+    for x, _, _ in test_loader:
+        x = x[:8].to(device)
+        recon0 = joint_0(x)
+        recon1 = joint_1(x)
+        break
 
+plot_path = plot_reconstruction(
+    x, recon0, "JointAutoencoder0", "PUC"
+)
 
-print("Model 0 Accuracy:", accuracy_score(y_true, y_pred_model_0))
-print("Model 1 Accuracy:", accuracy_score(y_true, y_pred_model_1))  
+plot_path = plot_reconstruction(
+    x, recon1, "JointAutoencoder1", "PUC"
+)
+
--- src/models/loss/autoencoder_loss.py
+++ src/models/loss/autoencoder_loss.py
@@ -1,25 +1,30 @@
 from src.utils.image_metrics import calculate_ssim_torch
-from sklearn.metrics import silhouette_score
 import numpy as np
+import torch.nn.functional as F
+from sklearn.metrics.pairwise import euclidean_distances
 
 
+print("Distância média entre embeddings:", mean_distance)
 def ssim_loss(recon_x, x):
     ssim = calculate_ssim_torch(recon_x, x)
     return 1 - ssim
 
-def silhouette_loss(cluster_a, cluster_b):
-    # cluster_a e cluster_b são tensores de forma (N, D)
-    # onde N é o número de amostras e D é a dimensão das features
-    # calculamos a silhouette score usando sklearn
-    cluster_a_np = cluster_a.cpu().detach().numpy()
-    cluster_b_np = cluster_b.cpu().detach().numpy()
-    
-    # Criamos um array de rótulos para as amostras
-    labels = np.array([0] * len(cluster_a_np) + [1] * len(cluster_b_np))
-    
-    # Concatenamos os clusters para calcular a silhouette score
-    combined_clusters = np.vstack((cluster_a_np, cluster_b_np))
-    
-    score = silhouette_score(combined_clusters, labels)
-    
-    return 1 - score  # Queremos minimizar a perda, então subtraímos de 1
+
+# Função que calcula a distância euclidiana entre os embeddings de dois clusters
+# 0 -> são iguais
+# 1 -> são completamente diferentes
+# como quero variar, faço -1 para inverter a escala
+def euclidean_distance_loss(cluster_a, cluster_b):
+
+    cluster_a = F.normalize(cluster_a, dim=1)
+    cluster_b = F.normalize(cluster_b, dim=1)
+
+    a = cluster_a.detach().cpu().numpy()
+    b = cluster_b.detach().cpu().numpy()
+
+    dist_matrix = euclidean_distances(a, b)
+    mean_distance = dist_matrix.mean()
+
+    normalized = mean_distance / 2
+
+    return -1 - normalized
